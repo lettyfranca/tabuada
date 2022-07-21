0688e0f73282d911aba933c72278aa933caaae3c
lettyfranca <leeffrance@gmail.com>
2022-07-20 21:14:15 -0300

    última alteração

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
 for(let i = 0; i <= 10; i++){
     document.write("<p>Tabuada do " + i + "</p>");
     for(let j = 0; j <= 10; j++){
+        let multiplicacao = i * j;
         document.write(i + " x " + j + " = " + (i * j) + "<br>");
     }
 }
